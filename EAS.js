8c6f05aa09c4f607069a41018c110c98951aff6c
TyGuy98 <LittleCharon@protonmail.com>
2024-09-25 21:27:40 +0100

    Update event handling
    I changed the original event handler to use
    event delegation to significantly reduce the
    number of event listeners.

--- EAS.js
+++ EAS.js
@@ -1,33 +1,68 @@
 const container = document.querySelector(".container");
-const resetBtn = document.querySelector("button");
-
+const resetBtn = document.querySelector("#resetBtn");
+const resizeBtn = document.querySelector("#resizeBtn");
+let size = 16;
 
 function createGrid(){
-    for (let i = 0; i < 256; i ++){
-        const div = document.createElement("div");
-        container.appendChild(div);
-       
     
-        div.addEventListener("mouseenter", (e) => {
-            div.style.backgroundColor = "red";
-        });
+    const totalDivs = size * size;
+    for (let i = 0; i < totalDivs; i ++){
+        const div = document.createElement("div");
+        div.classList.add("grid-item");
+        div.style.flex = `1 0 calc(100% / ${size})`;
+        container.appendChild(div);
     }
 }
 
 function resetGrid() {
+    removeGrid();
+    createGrid(size);
+}
+
+
+function resizeGrid(input){
+    size = parseInt(input);
+    removeGrid();
+    createGrid(size);
+}
+
+function validateInput(input){
+    if (isNaN(input) || input > 100){
+        return false;
+    }
+
+    return true;
+}
+
+function removeGrid(){
     const children = document.querySelectorAll(".container *")
     children.forEach(child => {
         child.remove();
    });
-   createGrid();
 }
+
+container.addEventListener("mouseenter", (e) => {
+    if (e.target.classList.contains("grid-item")) {
+        e.target.style.backgroundColor = "red";
+    }
+    
+}, true);
 
 resetBtn.addEventListener("click", function (e) {
         resetGrid();
     });
 
+resizeBtn.addEventListener("click", function (e) {
+    let input = "";
+    do {
+        input =  prompt("Please enter a number (100 max)");
+    }while(!(validateInput(input)));
 
-createGrid();
+       resizeGrid(input)
+    });
+
+
+createGrid(size);
 
 
 
